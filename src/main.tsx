5bcfc35923290c5519fe379c23cf9f9c6068666b
Mahmoudamin11 <mahmoudamin123321@gmail.com>
2024-03-02 13:22:36 +0200

    fix router

--- src/main.tsx
+++ src/main.tsx
@@ -2,39 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {   RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.tsx'
-import OpenedProd from './components/OpenedProd.tsx'
-import AllProducts from './pages/AllProducts.tsx'
-import InCart from './pages/InCart.tsx'
-
-const router = createBrowserRouter([
-  {
-    path: "/Living-furniture/",
-    element: <App />,
-    children: [
-      {
-        path: "/Living-furniture/",
-        element: <Home />,
-      },
-      {
-        path: "/Living-furniture/product",
-        element: <OpenedProd />,
-      },
-      {
-        path: "/Living-furniture/AllProducts",
-        element: <AllProducts />,
-      },
-      {
-        path: "/Living-furniture/InCart",
-        element: <InCart />,
-      },
-    ],
-  },
-]);
+import {  HashRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      
+      <App />
+      
+    </Router>
   </React.StrictMode>,
 )
