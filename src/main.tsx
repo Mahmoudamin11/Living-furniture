c27d8d62bdb81f982a1db8453d0957bdcc56fa5f
Mahmoudamin11 <mahmoudamin123321@gmail.com>
2024-03-02 13:08:21 +0200

    using routerProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {  HashRouter as Router } from 'react-router-dom'
+import {   RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.tsx'
+import OpenedProd from './components/OpenedProd.tsx'
+import AllProducts from './pages/AllProducts.tsx'
+import InCart from './pages/InCart.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/Living-furniture/",
+    element: <App />,
+    children: [
+      {
+        path: "/Living-furniture/",
+        element: <Home />,
+      },
+      {
+        path: "/Living-furniture/product",
+        element: <OpenedProd />,
+      },
+      {
+        path: "/Living-furniture/AllProducts",
+        element: <AllProducts />,
+      },
+      {
+        path: "/Living-furniture/InCart",
+        element: <InCart />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      
-      <App />
-      
-    </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
